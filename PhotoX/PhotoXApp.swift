c49a51def20e454cace5e86d82e563d99595f382
Zhang Yuf <zhangyufan@didiglobal.com>
2023-09-26 15:48:43 +0800

    Initial Commit

---
+++ PhotoX/PhotoXApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoXApp.swift
+//  PhotoX
+//
+//  Created by Zhang Yuf on 2023/9/26.
+//
+
+import SwiftUI
+
+@main
+struct PhotoXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
